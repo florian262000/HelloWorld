7282bb5457bf985ced119bf856b84daccd65db3d
Florian <34872915+florian262000@users.noreply.github.com>
2021-04-28 09:43:48 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World4");
+            Console.WriteLine("Hello World5");
         }
     }
 }
